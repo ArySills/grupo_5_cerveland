1588c65f178ecba21b92e0a5f3eb02d4ce674843
Celeste Zapiola Aguirre <czapiola@ml.com>
2021-07-28 20:48:17 -0300

    se agrega campo imagen en json

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,11 +6,11 @@ const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const controller= {
 detail: (req, res)=>{
 const id = req.params.id;
 const user = users.find((us) => us.id == id);
-
 const viewData = {
     user
 }
