568c1872d82d5f799b8c341d6d1d29acfbd4835f
Celeste Zapiola Aguirre <czapiola@ml.com>
2021-08-01 21:09:12 -0300

    login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const {validationResult} = require ('express-validator') 
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controlador = {
-    login: (req, res)=>{res.render('users/register')}}
+    login: (req, res)=>{
+        let errors = validationResult(req);
+        if (!errores.isEmpty()){
+            return res.render('users/register',{mensajeDeError: errors.mapped()})
+        }else {res.redirect (303,'/')}}
+}
+
+
+   
+    module.exports = controlador;
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,13 @@
 const express = require ("express");
 const router = express.Router();
-const registerController = require ('../controllers/registerController');
+const loginController = require ('../controllers/loginController');
+const {check, body} = require ('express-validator');
+const validaciones = [
+    body('userName').notEmpty().withMessage("Debes completar el campo usuario"),
+    body('userEmail').notEmpty().withMessage("Debes completar con un email valido"),
+    body('userPassword').notEmpty().withMessage("Debes completar el campo contraseña")
+]
 
-router.post('/',registerController.create);
+router.post('/',validaciones,loginController.login);
 
 module.exports = router;
