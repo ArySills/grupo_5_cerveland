5031cb3c14325eda3b9d48e7bb5c31df16ec63bb
Celeste Zapiola Aguirre <czapiola@ml.com>
2021-08-07 12:37:52 -0300

    login con validacion de email y clave

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,33 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const {validationResult} = require ('express-validator') 
+const { validationResult } = require('express-validator')
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-const bcryptjs = require ('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controlador = {
-    login: (req, res)=>{
-              
-        let errors = validationResult(req); 
+    login: (req, res) => {
+
+        let errors = validationResult(req);
         let userLogged = '';
-        if (errors.isEmpty()){
-            for (let i = 0; i < users.length; i++ ){
-                if (users[i].userEmail == req.body.userEmail){
-                    if (bcryptjs.compareSync(req.body.userPassword, users[i].userPassword)){
-                        userLogged = users[i];
-                        break;
-                    }
+        if (errors.isEmpty()) {
+            console.log("No hay errores");
+            for (let i = 0; i < users.length; i++) {
+                console.log("Entro al for");
+                if (users[i].userEmail == req.body.userEmail && bcryptjs.compareSync(req.body.userPassword, users[i].userPassword)) 
+                {
+                    userLogged = users[i];
+                    console.log(userLogged);
+                    break;
                 }
+                else userLogged = undefined;
             }
-            if (userLogged == undefined){
-                return res.render('users/register', {errors: [{msg: 'Credenciales invalidas'}]})
-            } else {req.session.usuarioLogueado = userLogged
-                    return res.redirect (303,'/')}}
-        else {res.render('users/register',{errors: errors.errors})}
+                if (userLogged == undefined) {
+            console.log(userLogged);
+            return res.render('users/register', { errors: [{ msg: 'Credenciales invalidas' }] })
+        } else {
+            req.session.usuarioLogueado = userLogged
+            return res.redirect(303, '/')
+        }
+    }
+        else { res.render('users/register', { errors: errors.errors }) }
 }}
 
-   
-    module.exports = controlador;
+
+module.exports = controlador;
