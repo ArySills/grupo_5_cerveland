2e4ca1990ec3ff836d6400b000aff9c87c9e8c7a
ArySills <ary.sills@gmail.com>
2021-09-22 19:15:03 -0300

    validaciones front form register

--- public/js/register.js
+++ public/js/register.js
@@ -3,25 +3,44 @@
 let form = document.querySelector("form.form-container")
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    
-    /*
+let ulErrors = document.querySelector('div.errors ul');     
+ulErrors.innerHTML = "";
+
     let errors = [];
 
-    let firstName = document.querySelector("input.form-container-firstName")
-    if(firstName.value == ''){
-        errors.push('El campo nombre es obligatorio');
-    } else if(firstName.value.length < 2){
-        errors.push('El campo nombre debe tener al menos 2 caracteres');
+    let firstName = document.querySelector("#firstName")
+    let lastName = document.querySelector("#lastName")
+    let userEmail = document.querySelector("#userEmail")
+    
+    if(firstName.value.trim() == '' || lastName.value.trim() == ''){
+        errors.push('Los campos nombre y apellido son obligatorios');
+    } else if(firstName.value.length < 2 || lastName.value.length < 2 ){
+        errors.push('Los campos nombre y apellido deben tener al menos 2 caracteres');
     }
     if(errors.length > 0){
         e.preventDefault();
-        let ulErrors = document.querySelector('div.errors ul');
+
         for (let i=0; i < errors.length; i++){
             ulErrors.innerHTML += "<li>" + errors[i] + "</li>"
         }
-    } */
-})
+    } 
+    if(userEmail.value.trim() == ''){
+        errors.push('El campo email es obligatorio');
+    } else if (userEmail.value )
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+}) 
 }
 )
