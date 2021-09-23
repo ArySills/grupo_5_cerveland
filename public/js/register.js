2b2c5b7f94124a2ea51894fbff481d39c9630595
Celeste Maria Del Mar Zapiola Aguirre <czapiola@ml.com>
2021-09-22 21:35:33 -0300

    validacion register

--- public/js/register.js
+++ public/js/register.js
@@ -8,10 +8,16 @@ ulErrors.innerHTML = "";
 
     let errors = [];
 
-    let firstName = document.querySelector("#firstName")
-    let lastName = document.querySelector("#lastName")
-    let userEmail = document.querySelector("#userEmail")
-    
+    let firstName = document.querySelector("#firstName");
+    let lastName = document.querySelector("#lastName");
+    let userEmail = document.querySelector("#userEmail");
+    let expresion1 = /\w+@\w+\.+[a-z]/;
+    let userPassword = document.querySelector("#userPassword");
+    let profileImage = document.querySelector("#profileImage");
+    let expresion2= /\.(jpg|png|gif)$/i;
+
+
+   //nombre y apellido 
     if(firstName.value.trim() == '' || lastName.value.trim() == ''){
         errors.push('Los campos nombre y apellido son obligatorios');
     } else if(firstName.value.length < 2 || lastName.value.length < 2 ){
@@ -24,23 +30,32 @@ ulErrors.innerHTML = "";
             ulErrors.innerHTML += "<li>" + errors[i] + "</li>"
         }
     } 
+    //email
     if(userEmail.value.trim() == ''){
         errors.push('El campo email es obligatorio');
-    } else if (userEmail.value )
+    }else if (!expresion1.test(userEmail)){
+        errors.push('Debes ingresar un email válido');
+    }
+      //password
+      if(userPassword.value.trim() == ''){
+        errors.push('La contraseña es obligatoria');
+    } else if(userPassword.value.length < 8){
+        errors.push('La contraseña debe tener al menos 8 caracteres');
+    }
+    if(errors.length > 0){
+        e.preventDefault();
 
+        for (let i=0; i < errors.length; i++){
+            ulErrors.innerHTML += "<li>" + errors[i] + "</li>"
+        }
+    } 
+      //imagen
 
-
-
-
-
-
-
-
-
-
-    
-
-
+      if (!expresion2.test(profileImage.name)){
+        errors.push('El archivo a adjuntar no es una imagen');
+      }else {
+        alert('Imagen cargada')                
+    }
 }) 
 }
 )
